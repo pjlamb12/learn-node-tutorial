1ad8e32a302a6a0a18f71c1f5da6fd97fa4255ee
Preston Lamb <preston.j.lamb@gmail.com>
2017-06-17 00:14:44 -0600

    removed node_modules so I could remove them from source control

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,10 @@ const router = express.Router();
 
 // Do work here
 router.get('/', (req, res) => {
-  res.send('Hey! It works!');
+  // res.send('Hey! It works!');
+  res.render('hello', {
+    name: 'preston'
+  });
 });
 
 module.exports = router;

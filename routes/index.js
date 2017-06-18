7f4ec324971e5c1d39c3b7d3ea27a499e951aded
Preston Lamb <preston.j.lamb@gmail.com>
2017-06-17 22:05:37 -0600

    Set up Store model

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const storeController = require('../controllers/storeController');
 
 // Do work here
-router.get('/', storeController.myMiddleware, storeController.homePage);
+router.get('/', storeController.homePage);
 
 module.exports = router;

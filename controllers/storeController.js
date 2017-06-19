67ffe1f94e55631d873b81f4e31f9c0a1e048361
Preston Lamb <preston.j.lamb@gmail.com>
2017-06-18 23:45:08 -0600

    Edit a store

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -3,19 +3,34 @@ const Store = mongoose.model('Store');
 
 exports.homePage = (req, res) => {
   res.render('index')
-}
+};
 
 exports.addStore = (req, res) => {
   res.render('editStore', { title: 'Add Store'});
-}
+};
 
 exports.createStore = async (req, res) => {
   const store = await (new Store(req.body)).save();
   req.flash('success', `Successfully created ${store.name}. Care to leave a review?`);
   res.redirect(`/store/${store.slug}`);
-}
+};
+
+exports.updateStore = async (req, res) => {
+  const store = await Store.findOneAndUpdate({ _id: req.params.id }, req.body, {
+    new: true, // return the new store and not the old one
+    runValidators: true
+  }).exec();
+  req.flash('success', `Successfully updated ${store.name}. <a href="/stores/${store.slug}">View Store --></a>`);
+  res.redirect(`/stores/${store._id}/edit`);
+};
 
 exports.getStores = async (req, res) => {
   const stores = await Store.find();
   res.render('stores', { title: 'Stores', stores });
-}
+};
+
+exports.editStore = async (req, res) => {
+  console.log('Hey here I am');
+  const store = await Store.findOne({ _id: req.params.id });
+  res.render('editStore', { title: `Edit ${store.name}`, store });
+};

d6ece35fbbfdaf0e390c63db6a4b298160287598
Preston Lamb <preston.j.lamb@gmail.com>
2017-06-19 15:21:50 -0600

    wrote aggregator for MongoDB; made a tags page

--- routes/index.js
+++ routes/index.js
@@ -5,19 +5,26 @@ const { catchErrors } = require('../handlers/errorHandlers');
 
 // Do work here
 router.get('/', catchErrors(storeController.getStores));
+
 router.get('/stores', catchErrors(storeController.getStores));
+
 router.get('/add', storeController.addStore);
 router.post('/add',
   storeController.upload,
   catchErrors(storeController.resize),
   catchErrors(storeController.createStore)
 );
+
 router.post('/add/:id',
   storeController.upload,
   catchErrors(storeController.resize),
   catchErrors(storeController.updateStore)
 );
+
 router.get('/stores/:id/edit', catchErrors(storeController.editStore));
 router.get('/stores/:slug', catchErrors(storeController.getStoreBySlug));
+
+router.get('/tags', catchErrors(storeController.getStoresByTag));
+router.get('/tags/:tag', catchErrors(storeController.getStoresByTag));
 
 module.exports = router;

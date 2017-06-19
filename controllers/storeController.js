f371c6169f804367b253ae144ddfbe74fc860ce2
Preston Lamb <preston.j.lamb@gmail.com>
2017-06-19 15:27:37 -0600

    get stores based on tag, or all stores, and do multiple queries at once.

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -74,7 +74,12 @@ exports.getStoreBySlug = async (req, res, next) => {
 }
 
 exports.getStoresByTag = async (req, res) => {
-  const tags = await Store.getTagsList();
   const tag = req.params.tag;
-  res.render('tag', { tags, tag, title: 'Tags' });
+  const tagQuery = tag || { $exists: true };
+  const tagsPromise = Store.getTagsList();
+  const storesPromise = Store.find({ tags: tagQuery });
+
+  const [tags, stores] = await Promise.all([ tagsPromise, storesPromise]);
+
+  res.render('tag', { tags, tag, stores, title: 'Tags' });
 }

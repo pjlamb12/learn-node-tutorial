5890c9ef391fcb5033395c39c29625ccb4be8210
Preston Lamb <preston.j.lamb@gmail.com>
2017-06-20 12:13:09 -0600

    Password reset flow

--- models/user.js
+++ models/user.js
@@ -19,7 +19,9 @@ const userSchema = new Schema({
     type: String,
     required: 'Please provide a name.',
     trim: true
-  }
+  },
+  resetPasswordToken: String,
+  resetPasswordExpires: Date
 });
 
 userSchema.virtual('gravatar').get(function() {

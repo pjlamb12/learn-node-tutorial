f588ac2a69d8a798acb0eb5bf39fb3b30942b9c1
Preston Lamb <preston.j.lamb@gmail.com>
2017-06-20 14:04:40 -0600

    Added API endpoint for searching by location

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -106,3 +106,21 @@ exports.searchStores = async (req, res) => {
   .limit(5);
   res.json(stores);
 };
+
+exports.mapStores = async (req, res) => {
+  const coordinates = [req.query.lng, req.query.lat].map(parseFloat);
+  const q = {
+    location: {
+      $near: {
+        $geometry: {
+          type: 'Point',
+          coordinates
+        },
+        $maxDistance: 10000
+      }
+    }
+  }
+
+  const stores = await Store.find(q).select('slug name location description').limit(10);
+  res.json(stores);
+};
